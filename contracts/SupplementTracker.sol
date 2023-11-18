252a1838be867bab0bd8c37c921a3894e58c2edc
brfeitoza <bfeitoza634cyt@gmail.com>
2023-11-18 12:41:51 -0300

    Add SupplementTracker contract

---
+++ contracts/SupplementTracker.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract SupplementTracker {
+    struct SupplementInfo {
+        string name;
+        string manufacturer;
+        uint proteinContent;
+        uint carbs;
+        uint fats;
+        string expiryDate;
+    }
+
+    mapping(uint => SupplementInfo) public supplements;
+    uint public productCount = 0;
+
+    function addSupplement(string memory _name, string memory _manufacturer, uint _proteinContent, uint _carbs, uint _fats, string memory _expiryDate) public {
+        supplements[productCount] = SupplementInfo(_name, _manufacturer, _proteinContent, _carbs, _fats, _expiryDate);
+        productCount++;
+    }
+
+    function getSupplementInfo(uint _productId) public view returns (SupplementInfo memory) {
+        require(_productId < productCount, "Product not found.");
+        return supplements[_productId];
+    }
+}
